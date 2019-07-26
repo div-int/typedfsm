fc0c938d3db2b33bd73e40eaf23866763ffcdeb5
Scott Moore <scott.moore@scottmooresoftware.com>
2019-07-26 17:39:14 +0100

    Change index.ts example script to use new events

--- src/index.ts
+++ src/index.ts
@@ -21,39 +21,48 @@ const enum GhostStates {
 const ghostState = new Typed.FSM<GhostStates>(GhostStates.Waiting);
 
 ghostState
-  .from(GhostStates.Waiting, 'Wait')
-  .to(GhostStates.Chasing, 'Chase')
-  .toFrom(GhostStates.Paused, 'Pause');
+  .from(GhostStates.Waiting)
+  .to(GhostStates.Chasing)
+  .to(GhostStates.Scatter)
+  .toFrom(GhostStates.Paused);
 
-// ghostState
-//   .from(GhostStates.Chasing, 'Chase')
-//   .toFrom(GhostStates.Scatter, 'Scatter')
-//   .toFrom(GhostStates.Frightened, 'Frighten')
-//   .toFrom(GhostStates.Paused, 'Pause');
+ghostState
+  .from(GhostStates.Chasing)
+  .toFrom(GhostStates.Scatter)
+  .toFrom(GhostStates.Frightened)
+  .toFrom(GhostStates.Paused);
 
-// ghostState
-//   .from(GhostStates.Scatter, 'Scatter')
-//   .toFrom(GhostStates.Frightened, 'Frighten')
-//   .toFrom(GhostStates.Paused, 'Pause');
+ghostState
+  .from(GhostStates.Scatter)
+  .toFrom(GhostStates.Frightened)
+  .toFrom(GhostStates.Paused);
 
-// ghostState
-//   .from(GhostStates.Frightened, 'Frighten')
-//   .to(GhostStates.Eaten, 'Eaten')
-//   .toFrom(GhostStates.Paused, 'Pause');
+ghostState
+  .from(GhostStates.Frightened)
+  .to(GhostStates.Eaten)
+  .toFrom(GhostStates.Paused);
 
-// ghostState
-//   .from(GhostStates.Eaten, 'Eaten')
-//   .to(GhostStates.Scatter, 'Scatter')
-//   .to(GhostStates.Chasing, 'Chase')
-//   .toFrom(GhostStates.Paused, 'Pause');
+ghostState
+  .from(GhostStates.Eaten)
+  .to(GhostStates.Scatter)
+  .to(GhostStates.Chasing)
+  .toFrom(GhostStates.Paused);
 
-// ghostState.from(GhostStates.Paused, 'Pause');
+ghostState
+  .from(GhostStates.Frightened)
+  .to(GhostStates.Eaten)
+  .to(GhostStates.Paused);
 
-// ghostState
-//   .from(GhostStates.Frightened, 'Frighten')
-//   .to(GhostStates.Eaten, 'Eat')
-//   .to(GhostStates.Paused, 'Pause');
+ghostState.OnPreChange = (from: GhostStates, to: GhostStates): boolean => {
+  console.log(`Attempting to change from ${from} to ${to}`);
+  return true;
+};
 
-console.log(`Current ghost state = ${ghostState.currentState}`);
+ghostState.OnPostChange = (from: GhostStates, to: GhostStates): boolean => {
+  console.log(`We are going to change from ${from} to ${to}`);
+  return true;
+};
 
 ghostState.debug();
+console.log(ghostState.change(GhostStates.Eaten));
+console.log(ghostState.change(GhostStates.Scatter));
