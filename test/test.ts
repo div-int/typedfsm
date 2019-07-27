dea41ce0d4b2ee7dbd770b7377800e81a0cbf8f0
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-07-27 20:49:53 +0100

    Add coveralls coverage script

--- test/test.ts
+++ test/test.ts
@@ -10,64 +10,72 @@ const enum GhostStates {
   Paused = 'Paused',
 }
 
-const ghostState = new Typed.FSM<GhostStates>(GhostStates.Waiting);
+// const ghostState = new Typed.FSM<GhostStates>(GhostStates.Waiting);
+
+// ghostState
+//   .from(GhostStates.Chasing)
+//   .toFrom(GhostStates.Scatter)
+//   .toFrom(GhostStates.Frightened)
+//   .toFrom(GhostStates.Paused);
+
+// ghostState
+//   .from(GhostStates.Scatter)
+//   .toFrom(GhostStates.Frightened)
+//   .toFrom(GhostStates.Paused);
+
+// ghostState
+//   .from(GhostStates.Frightened)
+//   .to(GhostStates.Eaten)
+//   .toFrom(GhostStates.Paused);
+
+// ghostState
+//   .from(GhostStates.Eaten)
+//   .to(GhostStates.Scatter)
+//   .to(GhostStates.Chasing)
+//   .toFrom(GhostStates.Paused);
+
+// ghostState.from(GhostStates.Paused);
+
+// ghostState
+//   .from(GhostStates.Frightened)
+//   .to(GhostStates.Eaten)
+//   .to(GhostStates.Paused);
+
+let ghostState: Typed.FSM<GhostStates>;
+
+ghostState = new Typed.FSM<GhostStates>(GhostStates.Waiting);
+
+describe('ghostState = new Typed.FSM<GhostStates>(GhostStates.Waiting)', () => {
+  it('ghostState.currentState === GhostStates.Waiting', () => {
+    expect(ghostState.currentState).to.equal(GhostStates.Waiting);
+  });
+});
 
 ghostState
   .from(GhostStates.Waiting)
   .to(GhostStates.Chasing)
   .toFrom(GhostStates.Paused);
 
-ghostState
-  .from(GhostStates.Chasing)
-  .toFrom(GhostStates.Scatter)
-  .toFrom(GhostStates.Frightened)
-  .toFrom(GhostStates.Paused);
-
-ghostState
-  .from(GhostStates.Scatter)
-  .toFrom(GhostStates.Frightened)
-  .toFrom(GhostStates.Paused);
-
-ghostState
-  .from(GhostStates.Frightened)
-  .to(GhostStates.Eaten)
-  .toFrom(GhostStates.Paused);
-
-ghostState
-  .from(GhostStates.Eaten)
-  .to(GhostStates.Scatter)
-  .to(GhostStates.Chasing)
-  .toFrom(GhostStates.Paused);
-
-ghostState.from(GhostStates.Paused);
-
-ghostState
-  .from(GhostStates.Frightened)
-  .to(GhostStates.Eaten)
-  .to(GhostStates.Paused);
-
-describe('const ghostState = new Typed.FSM<GhostStates>(GhostStates.Waiting);', () => {
-  it('Create GhostState FSM', () => {
-    expect(ghostState.currentState).to.equal(GhostStates.Waiting);
-  });
-});
-
-describe('[Paused] <==> [Waiting] ==> [Chasing]', () => {
-  it('Change state [Waiting] ==> [Paused]', () => {
+describe('ghostState\n\t.from(GhostStates.Waiting)\n\t.to(GhostStates.Chasing)\n\t.toFrom(GhostStates.Paused);', () => {
+  it('ghostState.change(GhostStates.Paused) === GhostStates.Paused', () => {
     ghostState.change(GhostStates.Paused);
     expect(ghostState.currentState).to.equal(GhostStates.Paused);
   });
-  it('Change state [Waiting] <== [Paused]', () => {
+  it('ghostState.change(GhostStates.Waiting) === GhostStates.Waiting)', () => {
     ghostState.change(GhostStates.Waiting);
     expect(ghostState.currentState).to.equal(GhostStates.Waiting);
   });
-  it('Change state [Waiting] ==> [Chasing]', () => {
+  it('ghostState.change(GhostStates.Chasing) === GhostStates.Chasing', () => {
     ghostState.change(GhostStates.Chasing);
     expect(ghostState.currentState).to.equal(GhostStates.Chasing);
   });
-  it("Don't change state [Waiting] ==> [Scatter]", () => {
+  it('ghostState.reset() === GhostStates.Waiting', () => {
     ghostState.reset();
     expect(ghostState.currentState).to.equal(GhostStates.Waiting);
+  });
+  it('ghostState.canChange(GhostStates.Frightened) === false', () => {
+    const canChangeResult = ghostState.canChange(GhostStates.Frightened);
+    expect(canChangeResult).to.equal(false);
   });
 });
 
