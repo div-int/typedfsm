d9f586019470a0f1a5a5e665bd4b72c21b10ffaa
Scott Moore (Scott Moore Software) <scott.moore@scottmooresoftware.com>
2019-07-31 21:19:16 +0100

    vs code doing strange things with long generic instantiation line

--- test/trafficlights.ts
+++ test/trafficlights.ts
@@ -1,64 +1,64 @@
 import { Typed } from '../src/typedfsm';
 import { expect } from 'chai';
 
-const enum TLStates {
-  Red = 'Waiting',
-  Amber = 'Chasing',
-  Green = 'Scatter',
-  RedAmber = 'Red + Amber',
+namespace TrafficLights {
+  export const enum States {
+    Red = 'Waiting',
+    Amber = 'Chasing',
+    Green = 'Scatter',
+    RedAmber = 'Red + Amber',
+  }
+
+  export const enum Actions {
+    Change = 'Change',
+    DoNothing = 'DoNothing',
+  }
+
+  const trafficLightState = new Typed.FSM<States, Actions>(States.Red);
+
+  trafficLightState.from(States.Red).to(States.RedAmber, Actions.Change);
+  trafficLightState.from(States.RedAmber).to(States.Green, Actions.Change);
+  trafficLightState.from(States.Green).to(States.Amber, Actions.Change);
+  trafficLightState.from(States.Amber).to(States.Red, Actions.Change);
+
+  describe('Test traffic light state machine.', () => {
+    it('Is the current/default state red?', () => {
+      trafficLightState.reset();
+      expect(trafficLightState.currentState).to.equal(States.Red);
+    });
+  });
+
+  describe('Change the traffic lights state.', () => {
+    it('It should now be red and amber?', () => {
+      trafficLightState.do(Actions.Change);
+      expect(trafficLightState.currentState).to.equal(States.RedAmber);
+    });
+
+    it('It should now be green?', () => {
+      trafficLightState.do(Actions.Change);
+      expect(trafficLightState.currentState).to.equal(States.Green);
+    });
+
+    it('It should now be amber?', () => {
+      trafficLightState.do(Actions.Change);
+      expect(trafficLightState.currentState).to.equal(States.Amber);
+    });
+
+    it('It should now be red?', () => {
+      trafficLightState.do(Actions.Change);
+      expect(trafficLightState.currentState).to.equal(States.Red);
+    });
+
+    it('It should now be red and amber again?', () => {
+      trafficLightState.do(Actions.Change);
+      expect(trafficLightState.currentState).to.equal(States.RedAmber);
+    });
+  });
+
+  describe('Do nothing to the traffic lights state.', () => {
+    it('It should still be red and amber?', () => {
+      trafficLightState.do(Actions.DoNothing);
+      expect(trafficLightState.currentState).to.equal(States.RedAmber);
+    });
+  });
 }
-
-const enum TLActions {
-  Change = 'Change',
-  DoNothing = 'DoNothing',
-}
-
-const trafficLightState = new Typed.FSM<TLStates, TLActions>(TLStates.Red);
-
-trafficLightState
-  .from(TLStates.Red, TLActions.Change)
-  .to(TLStates.RedAmber, TLActions.Change);
-trafficLightState.from(TLStates.RedAmber).to(TLStates.Green, TLActions.Change);
-trafficLightState.from(TLStates.Green).to(TLStates.Amber, TLActions.Change);
-trafficLightState.from(TLStates.Amber).to(TLStates.Red, TLActions.Change);
-
-describe('Test traffic light state machine.', () => {
-  it('Is the current/default state red?', () => {
-    trafficLightState.reset();
-    expect(trafficLightState.currentState).to.equal(TLStates.Red);
-  });
-});
-
-describe('Change the traffic lights state.', () => {
-  it('It should now be red and amber?', () => {
-    trafficLightState.do(TLActions.Change);
-    expect(trafficLightState.currentState).to.equal(TLStates.RedAmber);
-  });
-
-  it('It should now be green?', () => {
-    trafficLightState.do(TLActions.Change);
-    expect(trafficLightState.currentState).to.equal(TLStates.Green);
-  });
-
-  it('It should now be amber?', () => {
-    trafficLightState.do(TLActions.Change);
-    expect(trafficLightState.currentState).to.equal(TLStates.Amber);
-  });
-
-  it('It should now be red?', () => {
-    trafficLightState.do(TLActions.Change);
-    expect(trafficLightState.currentState).to.equal(TLStates.Red);
-  });
-
-  it('It should now be red and amber again?', () => {
-    trafficLightState.do(TLActions.Change);
-    expect(trafficLightState.currentState).to.equal(TLStates.RedAmber);
-  });
-});
-
-describe('Do nothing to the traffic lights state.', () => {
-  it('It should still be red and amber?', () => {
-    trafficLightState.do(TLActions.DoNothing);
-    expect(trafficLightState.currentState).to.equal(TLStates.RedAmber);
-  });
-});
